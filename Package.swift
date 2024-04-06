71053a10f973e4cba69244bbf179c7d8a1bbf2ca
wuyu <6262609@qq.com>
2024-04-06 19:20:04 +0800

    Move typealias PlatformColor into MaterialColor

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -14,10 +15,10 @@ let package = Package(
             name: "MaterialColor",
             targets: ["MaterialColor"]),
     ],
-    dependencies: [
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "MaterialColor"),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
