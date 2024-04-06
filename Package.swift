6f039e0a09ddf04a89c6abd87dba00c72722f9f8
wuyu <6262609@qq.com>
2024-04-06 21:01:20 +0800

    Refactor Package.swift to lower platform versions.

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "MaterialColor",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v12),
-        .tvOS(.v12),
-        .watchOS(.v5)
+        .macOS(.v10_10),
+        .iOS(.v8),
+        .tvOS(.v9),
+        .watchOS(.v2)
     ],
     products: [
         .library(
